47e719e46e86897f0f26b3d812bc7bf8b1015274
Solomon Li <solomon@unity3d.com>
2016-08-04 14:41:04 +0800

    Implement supersonic show video ads function.

---
+++ unity-ads-supersonic-ios/unity-ads-supersonic-ios/RVDelegate.h
@@ -0,0 +1,14 @@
+//
+//  RVDelegate.h
+//  unity-ads-supersonic-ios
+//
+//  Created by Solomon Li on 8/4/16.
+//  Copyright © 2016 Solomon Li. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Supersonic/Supersonic.h"
+
+@interface RVDelegate : NSObject <SupersonicRVDelegate>
+
+@end
--- unity-ads-supersonic-ios/unity-ads-supersonic-ios/ViewController.m
+++ unity-ads-supersonic-ios/unity-ads-supersonic-ios/ViewController.m
@@ -7,9 +7,12 @@
 //
 
 #import "ViewController.h"
+#import "Supersonic/Supersonic.h"
+#import "RVDelegate.h"
 
 @interface ViewController ()
-
+@property (nonatomic, strong) NSString* appKey;
+@property (nonatomic, strong) NSString* userId;
 @end
 
 @implementation ViewController
@@ -17,11 +20,25 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    RVDelegate* rvDelegate = [RVDelegate new];
+    _appKey = @"516c10fd";
+    _userId = @"customizableUserId";
+    [[Supersonic sharedInstance] setRVDelegate:rvDelegate];
+    [[Supersonic sharedInstance] initRVWithAppKey:_appKey withUserId:_userId];
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)showAdButtonTapped:(id)sender {
+    BOOL rvAvailable = [[Supersonic sharedInstance] isAdAvailable];
+    if(rvAvailable){
+        [[Supersonic sharedInstance] showRVWithViewController:self];
+    }
 }
 
 @end
