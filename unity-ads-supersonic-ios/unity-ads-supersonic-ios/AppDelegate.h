96c8c4e5730e1d7bd745e43b1e717cf646684ff9
Solomon Li <solomon@unity3d.com>
2016-08-04 11:25:28 +0800

    Add supersonic framework and their official documents.

---
+++ unity-ads-supersonic-ios/unity-ads-supersonic-ios/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  unity-ads-supersonic-ios
+//
+//  Created by Solomon Li on 8/2/16.
+//  Copyright © 2016 Solomon Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ unity-ads-supersonic-ios/unity-ads-supersonic-ios/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  unity-ads-supersonic-ios
+//
+//  Created by Solomon Li on 8/2/16.
+//  Copyright © 2016 Solomon Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
